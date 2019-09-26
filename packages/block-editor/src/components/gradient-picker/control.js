aa9b7d3ee8fa11629145abe7d02453bc1d7f97a9
Jorge <jorge.costa@developer.pt>
2019-09-26 10:34:17 +0100

    Code refactor (+2 squashed commits) Squashed commits: [cc336649f] Refactor and improvements [f04445763] Add: Custom gradient component

--- packages/block-editor/src/components/gradient-picker/control.js
+++ packages/block-editor/src/components/gradient-picker/control.js
@@ -8,7 +8,7 @@ import { isEmpty } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { BaseControl } from '@wordpress/components';
+import { BaseControl, CustomGradientPicker } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 
@@ -17,7 +17,7 @@ import { useSelect } from '@wordpress/data';
  */
 import GradientPicker from './';
 
-export default function( { className, label = __( 'Gradient Presets' ), ...props } ) {
+export default function( { className, value, onChange, label = __( 'Gradient Presets' ), ...props } ) {
 	const gradients = useSelect( ( select ) => (
 		select( 'core/block-editor' ).getSettings().gradients
 	) );
@@ -35,9 +35,15 @@ export default function( { className, label = __( 'Gradient Presets' ), ...props
 				{ label }
 			</BaseControl.VisualLabel>
 			<GradientPicker
+				value={ value }
+				onChange={ onChange }
 				className="block-editor-gradient-picker-control__gradient-picker-presets"
 				gradients={ gradients }
 				{ ...props }
+			/>
+			<CustomGradientPicker
+				value={ value }
+				onChange={ onChange }
 			/>
 		</BaseControl>
 	);
