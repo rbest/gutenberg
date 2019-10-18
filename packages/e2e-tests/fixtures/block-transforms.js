334fae7678b1c4397aba371b68037497bc758ea4
Jorge <jorge.costa@developer.pt>
2019-10-18 14:52:59 +0100

    Fix: Cover to video and image transforms.

--- packages/e2e-tests/fixtures/block-transforms.js
+++ packages/e2e-tests/fixtures/block-transforms.js
@@ -121,8 +121,6 @@ export const EXPECTED_TRANSFORMS = {
 	core__cover__gradient: {
 		availableTransforms: [
 			'Group',
-			'Image',
-			'Video',
 		],
 		originalBlock: 'Cover',
 	},
@@ -137,6 +135,13 @@ export const EXPECTED_TRANSFORMS = {
 		availableTransforms: [
 			'Group',
 			'Video',
+		],
+		originalBlock: 'Cover',
+	},
+
+	'core__cover__solid-color': {
+		availableTransforms: [
+			'Group',
 		],
 		originalBlock: 'Cover',
 	},
