44084cecef7901ae7b8d71aeac2d004babbcae4e
Andrei Draganescu <andrei.draganescu@automattic.com>
2019-10-28 10:38:59 +0200

    removes changes to toolbar

--- packages/components/src/toolbar/index.js
+++ packages/components/src/toolbar/index.js
@@ -42,7 +42,7 @@ import ToolbarContainer from './toolbar-container';
  *
  * @return {WPComponent} The rendered component.
  */
-function Toolbar( { controls = [], children, className, isCollapsed, icon, label, showLabel, dropDownRef, ...otherProps } ) {
+function Toolbar( { controls = [], children, className, isCollapsed, icon, label, ...otherProps } ) {
 	if (
 		( ! controls || ! controls.length ) &&
 		! children
@@ -60,16 +60,11 @@ function Toolbar( { controls = [], children, className, isCollapsed, icon, label
 		return (
 			<DropdownMenu
 				hasArrowIndicator
-				dropDownRef={ dropDownRef }
 				icon={ icon }
 				label={ label }
 				controls={ controlSets }
 				className={ classnames( 'components-toolbar', className ) }
-				showLabel={ showLabel }
-				{ ...otherProps }
-			>
-				{ children }
-			</DropdownMenu>
+			/>
 		);
 	}
 
