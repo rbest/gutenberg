7abdc2ea05d8d8a6a3d2c76309be42e3130a7ab4
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2019-11-13 10:10:07 +0100

    Update newly added stories to work with Storyshots

--- storybook/test/index.js
+++ storybook/test/index.js
@@ -6,17 +6,17 @@ import path from 'path';
 
 initStoryshots( {
 	configPath: path.resolve( __dirname, '../' ),
-	test: snapshotWithOptions( ( { kind } ) => ( {
+	test: snapshotWithOptions( ( story ) => ( {
 		// We need to mock refs for some stories which use them.
 		// @see https://reactjs.org/blog/2016/11/16/react-v15.4.0.html#mocking-refs-for-snapshot-testing
 		// @see https://github.com/storybookjs/storybook/tree/master/addons/storyshots/storyshots-core#using-createnodemock-to-mock-refs
-		createNodeMock: () => {
-			if ( kind === 'Components|ClipboardButton' ) {
+		createNodeMock: ( element ) => {
+			if ( story.kind === 'Components|ClipboardButton' ) {
 				return {
 					firstChild: document.createElement( 'button' ),
 				};
 			}
-			return null;
+			return element.type && document.createElement( element.type );
 		},
 	} ) ),
 } );
