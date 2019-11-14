ef2399caecab582ee1ced38a22adaeb48c43494f
Jon Q <hello@jonquach.com>
2019-11-14 15:14:24 +0100

    Setting up Playground x Bravas

---
+++ packages/bravas/src/index.js
@@ -0,0 +1,22 @@
+/**
+ * WordPress dependencies
+ */
+// import { PluginSidebar } from '@wordpress/edit-post';
+// import { registerPlugin } from '@wordpress/plugins';
+// import '@wordpress/element';
+
+window.hello = 'omg';
+
+// registerPlugin( 'my-plugin-sidebar', {
+// 	render() {
+// 		return (
+// 			<PluginSidebar
+// 				name="my-plugin-sidebar"
+// 				icon="admin-post"
+// 				title="Hello"
+// 			>
+// 				Meta Fields
+// 			</PluginSidebar>
+// 		);
+// 	},
+// } );
